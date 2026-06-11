af76dc739532d4ae434b18ae2881a752b67215b2
JebobaTea <73501717+JebobaTea@users.noreply.github.com>
2026-06-11 09:42:45 +0800

    Update diagnose.py

--- diagnose.py
+++ diagnose.py
@@ -33,25 +33,3 @@ try:
 finally:
     # vehicle.send_signal(bytes([0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]))
     vehicle.kill()
-from utils.Control import Vehicle
-import time
-
-vehicle = Vehicle()
-print("Woke up peripherals")
-
-print("Forward only")
-for i in range(20):
-    vehicle.send_signal(vehicle.control_to_bytes(0, 0.1))
-    time.sleep(0.1)
-
-input("Breakpoint")
-print("Left")
-for i in range(20):
-    vehicle.send_signal(vehicle.control_to_bytes(-0.5, 0.1))
-    time.sleep(0.1)
-
-input("Breakpoint")
-print("Right")
-for i in range(20):
-    vehicle.send_signal(vehicle.control_to_bytes(0.5, 0.1))
-    time.sleep(0.1)
